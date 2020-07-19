25cba964807b691827fce7c99dae108996fb0c70
svnsaitejagit <60354610+svnsaitejagit@users.noreply.github.com>
2020-07-19 18:05:38 +0530

    Add files via upload

---
+++ CollectionCoustemImplementation.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+ 
+public class ListElements<E> 
+{
+    
+    private int size = 0;
+     
+private static final int MINIMUM_ELEMENTS = 10;
+     
+    
+    private Object elements[];
+ 
+    
+    public ListElements() {
+        elements = new Object[MINIMUM_ELEMENTS];
+    }
+ 
+    
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size++] = e;
+    }
+     
+    
+    
+    public E get(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+        }
+        return (E) elements[i];
+    }
+     
+    
+   
+    public E remove(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+        }
+        Object item = elements[i];
+        int numberofelements = elements.length - ( i + 1 ) ;
+        System.arraycopy( elements, i + 1, elements, i, numElts ) ;
+        size--;
+        return (E) item;
+    }
+     
+        public int size() {
+        return size;
+    }
+     
+        public String toString() 
+    {
+         StringBuilder sb = new StringBuilder();
+         sb.append('[');
+         for(int i = 0; i < size ;i++) {
+             sb.append(elements[i].toString());
+             if(i<size-1){
+                 sb.append(",");
+             }
+         }
+         sb.append(']');
+         return sb.toString();
+    }
+     
+    private void ensureCapacity() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+}
+public class CollectionCoustemImplementation{
+public static void main(String args[]){
+ListElements<Integer> list=new ListElements<> ();
+list.add();
+list.get();
+list.remove();
+list.size();
+list.toString();
+list.ensureCapacity();
+}}
